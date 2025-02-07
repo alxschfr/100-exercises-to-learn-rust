342672021f98cef7c2c5a2a2050338e84480037a
Alex <alex.schaefer.drums@gmail.com>
2025-02-07 19:00:33 +0100

    commit solution

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,19 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut sum = 1;
+    let mut i = 1;
+
+    if n <= 1 {
+        return 1;
+    } else {
+        while i <= n {
+            sum *= i;
+            i += 1;
+        }
+        return sum
+    }
+
 }
 
 #[cfg(test)]
