c5f6c26bcc000ea745f278de95920660a5da277d
Alex <alex.schaefer.drums@gmail.com>
2025-03-05 15:56:01 +0100

    commit solution

--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -1,6 +1,6 @@
 // TODO: Implement `TryFrom<String>` and `TryFrom<&str>` for `Status`.
 //  The parsing should be case-insensitive.
-
+use std::convert::TryFrom;
 #[derive(Debug, PartialEq, Clone)]
 enum Status {
     ToDo,
@@ -8,6 +8,28 @@ enum Status {
     Done,
 }
 
+impl TryFrom<String> for Status {
+    type Error = &'static str;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Status::Done),
+            _ => Err("Invalid status value")
+        }
+    }
+}
+impl TryFrom<&str> for Status {
+    type Error = &'static str;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Status::Done),
+            _ => Err("Invalid status value")
+        }
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
